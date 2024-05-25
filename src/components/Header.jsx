e9747d296a3daf1d627ea3c02a177306dc76de87
arslanblcn <arslan.bilecen@gmail.com>
2024-05-26 00:11:21 +0300

    Navbar updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import './header.css'; // Import your custom CSS for styling
 const CurvedMenu = () => {
   return (
     <div className="curved-menu-container">
+        <span className="logo">Turna Labs</span>
       <Menu mode="horizontal">
         <Menu.Item key="home" icon={<HomeOutlined />}>
           Home
