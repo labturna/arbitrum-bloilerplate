7dc5b1bec7ef789a8649331e566d1dca0e71addb
arslanblcn <arslan.bilecen@gmail.com>
2024-05-25 23:54:42 +0300

    Updated page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CurvedMenu from './components/Header';
+import './App.css'
+const App = () => {
+  return (
+    <div className='background-image-container'>
+      {/* Other content of your application */}
+      <CurvedMenu />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Menu } from 'antd';
+import { HomeOutlined, UserOutlined, SettingOutlined } from '@ant-design/icons';
+import './header.css'; // Import your custom CSS for styling
+
+const CurvedMenu = () => {
+  return (
+    <div className="curved-menu-container">
+      <Menu mode="horizontal">
+        <Menu.Item key="home" icon={<HomeOutlined />}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="profile" icon={<UserOutlined />}>
+          Profile
+        </Menu.Item>
+        <Menu.Item key="settings" icon={<SettingOutlined />}>
+          Settings
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
+
+export default CurvedMenu;
